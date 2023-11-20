ad7b35c683a241c10e79bbe8c6e2d316898b5745
celina-ngth <ngeth.celina@gmail.com>
2023-11-20 17:00:31 +0100

    ordering-kiosk: init project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link> |
+  <router-link to="/menu">Menu</router-link>
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
---
+++ src/views/Menu.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { computed } from 'vue'
+import dataSource from '@/data.json'
+
+const data = computed(() => dataSource.menus)
+</script>
+
+<template>
+  <h1>Menu</h1>
+  <div v-for="item in data" :key="item.id">
+    <img :src="`/images/${item.image}`" :alt="item.name" style="width: 100px" />
+    <strong>{{ item.name }}</strong>
+    {{ item.price }}
+  </div>
+</template>
