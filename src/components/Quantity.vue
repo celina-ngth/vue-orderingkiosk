ae44f1dcd76a08a40c2cb1f1f1b0708e417a5e7a
celina-ngth <ngeth.celina@gmail.com>
2023-11-22 21:04:44 +0100

    feat(quantity): create component

---
+++ src/components/Quantity.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const quantity = ref(1)
+
+const emit = defineEmits(['update:qty'])
+
+function increment() {
+  quantity.value++
+  emit('update:qty', quantity.value)
+}
+
+function decrement() {
+  quantity.value--
+  emit('update:qty', quantity.value)
+}
+</script>
+
+<template>
+  <div class="flex items-center gap-2">
+    Quantity:
+    <button type="button" @click="decrement()" class="mt-2">-</button>
+    <div>{{ quantity }}</div>
+    <button type="button" @click="increment()" class="mt-2">+</button>
+  </div>
+</template>
