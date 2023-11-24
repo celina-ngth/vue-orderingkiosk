dc04765d09b6b71fec5dc58bf310f6b1b2bf6247
celina-ngth <ngeth.celina@gmail.com>
2023-11-24 23:44:40 +0100

    init json-server

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,15 +1,31 @@
 <script setup>
-import { computed } from 'vue'
-import dataSource from '@/data.json'
+import { ref, reactive, onMounted } from 'vue'
 import Product from '@/components/Product.vue'
 import Cart from '@/components/Cart.vue'
 
-const products = computed(() => dataSource.menus)
+let isLoading = ref(false)
+let products = reactive([])
+
+async function getProducts() {
+  try {
+    isLoading.value = true
+    const reponse = await fetch('http://localhost:3000/menus')
+    products = await reponse.json()
+
+    isLoading.value = false
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(async () => {
+  await getProducts()
+})
 </script>
 
 <template>
   <h1>Menu</h1>
-  <div class="grid grid-cols-4 gap-4">
+  <div v-if="!isLoading" class="grid grid-cols-4 gap-4">
     <Product v-for="product in products" :key="product.id" :product="product" />
   </div>
 
