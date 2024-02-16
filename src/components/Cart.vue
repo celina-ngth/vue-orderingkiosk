22c9f2b56bf8e4aa676841e8f2959d616da4f32a
celina-ngth <ngeth.celina@gmail.com>
2024-02-17 00:12:35 +0100

    feat(cart): display message when cart is empty

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -12,24 +12,30 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div
-    v-for="product in cart.$state.cart"
-    :key="product.id"
-    class="flex gap-4 py-2 border border-0 border-t-1 border-primary border-solid"
-  >
-    <span class="font-bold text-primary">x{{ product.quantity }}</span>
-    {{ product.name }} -
-    {{ formatPrice(product.amount) }}
+  <div v-if="cart.cart.length">
+    <div
+      v-for="product in cart.cart"
+      :key="product.id"
+      class="flex gap-4 py-2 border border-0 border-t-1 border-primary border-solid"
+    >
+      <span class="font-bold text-primary">x{{ product.quantity }}</span>
+      {{ product.name }} -
+      {{ formatPrice(product.amount) }}
 
-    <a @click="cart.removeFromCart(product.id)" class="cursor-pointer">
-      Supprimer
-    </a>
+      <a @click="cart.removeFromCart(product.id)" class="cursor-pointer">
+        Supprimer
+      </a>
+    </div>
+
+    <div
+      class="my-4 bg-primary text-lg font-bold text-white rounded py-1 px-2 flex justify-between"
+    >
+      <div>Total : {{ formatPrice(cart.total) }}</div>
+      <div>Nombre d'articles : {{ cart.nbItemsInCart }} articles</div>
+    </div>
   </div>
 
-  <div
-    class="my-4 bg-primary text-lg font-bold text-white rounded py-1 px-2 flex justify-between"
-  >
-    <div>Total : {{ formatPrice(cart.total) }}</div>
-    <div>Nombre d'articles : {{ cart.nbItemsInCart }} articles</div>
+  <div v-else class="text-primary text-lg font-bold">
+    Votre panier est vide.
   </div>
 </template>
